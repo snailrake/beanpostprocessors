cc70041d10394c71659309f9ceaf11101af4fe16
Viktor Dyukov <snailrake@mail.ru>
2025-04-08 11:20:13 +0400

    Initial commit

---
+++ src/main/java/com/example/beanpostprocessors/BeanpostprocessorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.beanpostprocessors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanpostprocessorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanpostprocessorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beanpostprocessors/BeanpostprocessorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beanpostprocessors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanpostprocessorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
